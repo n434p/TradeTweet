1d9fc3d853d840bb7d592059cdf7fffeaf3ccd9f
Nazar Kolomiets <nazar.kolomiets@gmail.com>
2017-02-03 11:18:18 +0200

    Add project files.

---
+++ TradeTweet/PluginForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TradeTweet
+{
+    partial class TradeTweet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cheersBtn = new PTLRuntime.NETScript.Controls.PTMCButton();
+            this.ptmcLabel1 = new PTLRuntime.NETScript.Controls.PTMCLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.cheersBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cheersBtn
+            // 
+            this.cheersBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cheersBtn.Checked = false;
+            this.cheersBtn.CustomForeColor = System.Drawing.Color.Empty;
+            this.cheersBtn.CustomForeShadowColor = System.Drawing.Color.Empty;
+            this.cheersBtn.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.cheersBtn.Group = null;
+            this.cheersBtn.Image = null;
+            this.cheersBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.cheersBtn.ImageCheked = null;
+            this.cheersBtn.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
+            this.cheersBtn.Location = new System.Drawing.Point(11, 51);
+            this.cheersBtn.MinimumDropDownSize = new System.Drawing.Size(0, 0);
+            this.cheersBtn.Name = "cheersBtn";
+            this.cheersBtn.Size = new System.Drawing.Size(261, 37);
+            this.cheersBtn.TabIndex = 0;
+            this.cheersBtn.TerceraButtonStyle = com.pfsoft.proftrading.controls.terceraStyle.TerceraButtonStyle.Standard;
+            this.cheersBtn.Text = "Cheers";
+            this.cheersBtn.Tooltip = null;
+            this.cheersBtn.TooltipSubButton = null;
+            this.cheersBtn.Click += new System.EventHandler(this.cheersBtn_Click);
+            // 
+            // ptmcLabel1
+            // 
+            this.ptmcLabel1.AutoSize = true;
+            this.ptmcLabel1.CustomBackColor = System.Drawing.Color.Empty;
+            this.ptmcLabel1.CustomForeColor = System.Drawing.Color.Empty;
+            this.ptmcLabel1.CustomShadowColor = System.Drawing.Color.Empty;
+            this.ptmcLabel1.Location = new System.Drawing.Point(95, 9);
+            this.ptmcLabel1.Name = "ptmcLabel1";
+            this.ptmcLabel1.ShowToolTip = false;
+            this.ptmcLabel1.Size = new System.Drawing.Size(63, 13);
+            this.ptmcLabel1.TabIndex = 1;
+            this.ptmcLabel1.Text = "Hello, world";
+            this.ptmcLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ptmcLabel1.TextAlignment = System.Drawing.StringAlignment.Near;
+            this.ptmcLabel1.TextLineAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // PluginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ptmcLabel1);
+            this.Controls.Add(this.cheersBtn);
+            this.Name = "PluginForm";
+            this.Text = "PluginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.cheersBtn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PTLRuntime.NETScript.Controls.PTMCButton cheersBtn;
+        private PTLRuntime.NETScript.Controls.PTMCLabel ptmcLabel1;
+    }
+}
---
+++ TradeTweet/PluginForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using PTLRuntime.NETScript;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PTLRuntime.NETScript.Controls;
+using com.pfsoft.proftrading.commons.external;
+using System.Drawing;
+
+namespace TradeTweet
+{
+    [Exportable]
+    public partial class TradeTweet : ComponentForm, IExternalComponent
+    {
+        public TradeTweet()
+        {
+            InitializeComponent();
+        }
+
+        private void cheersBtn_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Hello, world");
+        }
+
+        #region IExternalComponent
+        public Icon IconImage
+        {
+            get { return this.Icon; }
+        }
+
+        public string ComponentName
+        {
+            get { return "TradeTweet"; }
+        }
+
+        public string PanelHeader
+        {
+            get { return "TradeTweet"; }
+        }
+
+        public Control Content
+        {
+            get { return this; }
+        }
+
+        public void Populate()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+        #endregion
+    }
+}
